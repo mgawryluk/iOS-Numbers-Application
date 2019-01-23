ea48c21b89c33e5d652c440f8464da89a10f9a01
Michał <michal.gawryluk@icloud.com>
2019-01-23 21:21:43 +0100

    Setup a project

---
+++ iOS-Numbers-Application/iOS-Numbers-Application/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  iOS-Numbers-Application
+//
+//  Created by Michał on 23/01/2019.
+//  Copyright © 2019 Michał Gawryluk. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
